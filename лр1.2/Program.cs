354ba7224fa50ded7a9ef530128638d6411c5639
unknown <222909072092-11>
2025-09-12 16:07:04 +0300

    Файлы лабораторной 1 в ветку lab1
    las

---
+++ лр1.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace WordGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Заданный слог
+            string syllable = "ло";
+
+            int correctAnswers = 0;
+            int attempts = 0;
+
+            // Ввод слов до тех пор, пока не будет введена пустая строка
+            while (true)
+            {
+                Console.Write("Введите слово, содержащее слог \"{0}\" (или пустую строку для завершения): ", syllable);
+                string inputWord = Console.ReadLine();
+
+                // Проверка на пустую строку
+                if (inputWord == "")
+                {
+                    break;
+                }
+
+                attempts++;
+
+                // Проверка на наличие слога в слове
+                if (inputWord.ToLower().Contains(syllable))
+                {
+                    Console.WriteLine("Верно!");
+                    correctAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно! Слово не содержит слог \"{0}\".", syllable);
+                }
+            }
+
+            // Подсчет баллов
+            int score = (int)Math.Round((double)correctAnswers / attempts * 100);
+
+            // Вывод результатов
+            Console.WriteLine("\nРезультат:");
+            Console.WriteLine("Правильных ответов: {0}", correctAnswers);
+            Console.WriteLine("Попыток: {0}", attempts);
+            Console.WriteLine("Ваш балл: {0} из 100", score);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ лр1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четыре целых числа:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.SetCursorPosition((Console.WindowWidth - 1) / 2, Console.WindowHeight / 2 - 1); 
+
+            if (IsThreeDigitNumber(a))
+            {
+                PrintNumberWithColor(a);
+            }
+            if (IsThreeDigitNumber(b))
+            {
+                Console.SetCursorPosition((Console.WindowWidth - 1) / 2, Console.WindowHeight / 2); 
+                PrintNumberWithColor(b);
+            }
+            if (IsThreeDigitNumber(c))
+            {
+                Console.SetCursorPosition((Console.WindowWidth - 1) / 2, Console.WindowHeight / 2 + 1); 
+                PrintNumberWithColor(c);
+            }
+            if (IsThreeDigitNumber(d))
+            {
+                Console.SetCursorPosition((Console.WindowWidth - 1) / 2, Console.WindowHeight / 2 + 2); 
+                PrintNumberWithColor(d);
+            }
+
+            Console.ResetColor(); 
+            Console.ReadKey(); 
+        }
+
+        static bool IsThreeDigitNumber(int number)
+        {
+            return number >= 100 && number <= 999;
+        }
+
+        static void PrintNumberWithColor(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (number % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (number % 3 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.Write($"{number} ");
+        }
+    }
+}
+
+
